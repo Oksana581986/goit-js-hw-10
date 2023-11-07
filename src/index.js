6f414ebda9f49a87ff4c74c53096636be1519a76
Oksana581986 <oksanaotchych@gmail.com>
2023-11-07 21:50:02 +0200

    hwjs10done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
-import {fetchBreeds, fetchCatByBreed} from "./cat-api.js";
+import {fetchCatByBreed} from "./cat-api.js";
+import{fetchBreeds} from "./cat-api.js"
 
 
 axios.defaults.headers.common["x-api-key"] = "live_LVElnDG0qei7b46LgHfaN0g6p0Q8ltRmuwgyRcjBR890c1CX9OU8WlLveL28bWwO";
@@ -10,8 +11,6 @@ const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const errorElement = document.querySelector('.error');
-const URL = `https://api.thecatapi.com/v1`;
-
 
 fetch('https://api.thecatapi.com/v1/breeds')
     .then(response => {
@@ -36,15 +35,15 @@ fetch('https://api.thecatapi.com/v1/breeds')
 
     const showLoader = () => {
       loader.style.display = 'block';
-      breedSelect.style.display = true;
-      catInfo.style.display = true;
+      breedSelect.style.display = 'none';
+      catInfo.style.display = 'none';
       errorElement.style.display = 'none';
     };
     
     const hideLoader = () => {
       loader.style.display = 'none';
-      breedSelect.style.display = false;
-      catInfo.style.display = false;
+      breedSelect.style.display = 'block';
+      catInfo.style.display = 'block';
       errorElement.style.display = 'none';
     };
     
@@ -64,13 +63,19 @@ fetch('https://api.thecatapi.com/v1/breeds')
                 hideLoader();
                 errorElement.style.display = 'none';
                 const cat = response[0];
-
-        catInfo.innerHTML = `
-          <img src="${cat.url.reference_image_id}" alt="cat">
+              
+          catInfo.innerHTML = `
+          <div class="cat-info-container">
+          <div class="cat-photo-container">
+          <img class="cat-photo" src="${cat.url}" alt="cat">
+          </div>
+          <div class="cat-details">
           <h2>${cat.breeds[0].name}</h2>
           <p>Description: ${cat.breeds[0].description}</p>
           <p>Temperament: ${cat.breeds[0].temperament}</p>
-        `;
+          </div>
+          </div>
+          `;
       }) .catch(error => {
         hideLoader();
         showError("Error: " + error.message);
